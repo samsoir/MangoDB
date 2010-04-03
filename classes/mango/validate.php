1daf0644864669c8cd419bbfe3fac73d7a3f7332
Wouter <wouter.w@gmx.net>
2010-04-03 23:46:51 +0200

    Added isset() check to Mango_Validate to prevent notice exceptions

--- classes/mango/validate.php
+++ classes/mango/validate.php
@@ -4,9 +4,12 @@ class Mango_Validate extends Kohana_Validate {
 
 	public function offsetUnset($field)
 	{
-		unset($this->_labels[$field], $this->_filters[$field], $this->_rules[$field], $this->_callbacks[$field]);
+		if ( isset($this[$field]))
+		{
+			unset($this->_labels[$field], $this->_filters[$field], $this->_rules[$field], $this->_callbacks[$field]);
 
-		parent::offsetUnset($field);
+			parent::offsetUnset($field);
+		}
 	}
 
 } // End Mango_Validate
