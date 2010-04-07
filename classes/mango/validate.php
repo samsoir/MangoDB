67681d0c49b091683a259c120a11356da2f3c246
Wouter <wouter.w@gmx.net>
2010-04-07 20:43:29 +0200

    Rules etc can be set on empty fields - remove no matter if field itself is set

--- classes/mango/validate.php
+++ classes/mango/validate.php
@@ -4,10 +4,10 @@ class Mango_Validate extends Kohana_Validate {
 
 	public function offsetUnset($field)
 	{
+		unset($this->_labels[$field], $this->_filters[$field], $this->_rules[$field], $this->_callbacks[$field]);
+
 		if ( isset($this[$field]))
 		{
-			unset($this->_labels[$field], $this->_filters[$field], $this->_rules[$field], $this->_callbacks[$field]);
-
 			parent::offsetUnset($field);
 		}
 	}
